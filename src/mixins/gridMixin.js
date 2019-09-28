2dbafad29a502d69a436e24e7cf752444c6c4bc9
Apple <apple@Khank.local>
2019-09-28 17:43:15 +0700

    Fix: gridMixin type casting

--- src/mixins/gridMixin.js
+++ src/mixins/gridMixin.js
@@ -4,18 +4,19 @@ export default {
     ],
     computed: {
         styleSpan(){
-            const  { span: spanStyle }= this.styles;
+            const  { span: spanStyle } = this.styles;
 
             if (!spanStyle.responsive) {
-                return { span: spanStyle.fixed } ;
+                return { span: +spanStyle.fixed } ;
             }
 
             return {
-                xl: spanStyle.xl,
-                lg: spanStyle.lg,
-                md: spanStyle.md,
-                sm: spanStyle.sm,
-                xs: spanStyle.xs,
+                // span: false,
+                xl: +spanStyle.xl, // convert to number 
+                lg: +spanStyle.lg, // convert to number 
+                md: +spanStyle.md, // convert to number 
+                sm: +spanStyle.sm, // convert to number 
+                // xs: spanStyle.xs,
             }
         }
     },
